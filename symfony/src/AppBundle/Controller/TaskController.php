c84e5a72fbc1185552043c188bf7623dcdd9e630
ucze <micekluk@gmail.com>
2015-03-08 15:34:10 +0100

    Tasks with errors

---
+++ symfony/src/AppBundle/Controller/TaskController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Task;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Validator\Constraints\DateTime;
+
+class TaskController extends Controller
+{
+    /**
+     * @Route("/app/list", name="task")
+     */
+
+    public function indexAction()
+    {
+        $EN = $this->getDoctrine()->getManager();
+
+        $repo = $EN->getRepository('AppBundle:Task');
+
+        $tasks = $repo->findAll();
+
+
+        return $this->render('tasks/index.html.twig', array(
+            'tasks' => $tasks
+        ));
+    }
+
+}
+
+
+
