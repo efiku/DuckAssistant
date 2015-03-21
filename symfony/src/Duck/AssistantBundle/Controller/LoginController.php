addb0bf6655aad02e52ea36e62dd3134616b1cce
Krzysztof Pazdur <pazdurk@gmail.com>
2015-03-21 11:28:30 +0100

    Category Controller fix

--- symfony/src/Duck/AssistantBundle/Controller/LoginController.php
+++ symfony/src/Duck/AssistantBundle/Controller/LoginController.php
@@ -3,7 +3,6 @@
 namespace Duck\AssistantBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\BrowserKit\Request;
 
 class LoginController extends Controller
 {
